4ad66a631dc87f26539073dc286f0360aa913333
Andrés Valenzuela <andres@valsis.cl>
2025-01-09 19:24:22 -0300

    Inicial copia de Aion Es, header, cta, atributos, interior, exterior.

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  base: "/emkoo-hibrido",
+  site: "https://www.gacmotor.cl/emkoo-hibrido",
+});
